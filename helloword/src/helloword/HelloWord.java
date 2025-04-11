91e808b4cd351872ae8d984ffb4d568c73c9eac7
iagozandone <iagozandone@gmail.com>
2025-04-11 11:16:36 -0300

    :tada: Commit Inicial

---
+++ helloword/src/helloword/HelloWord.java
@@ -0,0 +1,12 @@
+package helloword;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Word!");
+		System.out.println("A turma Java 82 é TOP!");
+		
+	}
+
+}
