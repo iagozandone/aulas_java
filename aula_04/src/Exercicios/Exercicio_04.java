86a52febe8ad92eaf46b9e77071ceef12a233cf9
iagozandone <iagozandone@gmail.com>
2025-04-16 21:22:30 -0300

    :sparkles: Aula 04 - Laços de Repetições e Debug

---
+++ aula_04/src/Exercicios/Exercicio_04.java
@@ -0,0 +1,14 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio_04 {
+
+	public static void main(String[] args) {
+
+	       // Scanner para leitura de dados
+        Scanner leia = new Scanner(System.in);
+        
+        
+    }
+}
