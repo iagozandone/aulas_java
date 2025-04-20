aba937af610797811ac5b1c3752d7cae38e4a9db
iagozandone <iagozandone@gmail.com>
2025-04-20 16:18:01 -0300

    :sparkles: Aula 05 - Vetores e Matrizes

--- aula_05/src/aula_05/Vetores.java
+++ aula_05/src/aula_05/Vetores.java
@@ -9,67 +9,63 @@ public class Vetores {
 	public static void main(String[] args) {
 
 		Scanner leia = new Scanner(System.in);
-		
-		int vetorInteiros[] = {10, 15, 35, 70, 140, 280};
+
+		// Vetor de números inteiros inicializado com valores
+		int vetorInteiros[] = { 10, 15, 35, 70, 140, 280, 320, 480 };
+
+		// Vetor de Objetos Float, com tamanho 5
 		Float vetorReal[] = new Float[5];
-		
-		for(int indice = 0; indice < vetorInteiros.length; indice++) {
-			System.out.printf("vetorInteiros[%d] = %d\n", indice, vetorInteiros[indice]);	
+
+		// Vetor de Objetos String
+		String vetorStrings[] = { "Boxer", "Pastor Alemão", "Pinscher", "Husky Siberiano", "Corgi" };
+
+		// Variável float para armazenar a soma dos elementos vetorReal
+		float soma = 0;
+
+		// Laço for tradicional para exibir os dados do vetorInteiros
+		for (int indice = 0; indice < vetorInteiros.length; indice++) {
+			System.out.printf("\nvetorInteiros[%d] = %d", indice, vetorInteiros[indice]);
 		}
-		
-		System.out.println("");
-		
-		for(int indice = 0; indice < vetorReal.length; indice++) {
-			System.out.printf("Insira o valor em vetorReal[%d]: ", indice);
+
+		// Laço for tradicional para inserir dados no vetorReal
+		for (int indice = 0; indice < vetorReal.length; indice++) {
+			System.out.println("\nDigite um número Real: ");
 			vetorReal[indice] = leia.nextFloat();
 		}
-		
-		System.out.println("");
-		
-		for(float valor : vetorReal) {
-			System.out.printf("vetorReal = %f\n", valor);
-		}
-		
-		int soma = 0;
-		for(float valor : vetorReal) {
+
+		// Laço de Repetição for each para somar os dados do vetorReal
+		for (var valor : vetorReal) {
 			soma += valor;
 		}
-		
-		System.out.println("");
-		
+
+		// Exibindo o resultado da soma
 		System.out.println("Resultado da soma: " + soma);
-		
+
+		// Ordenando o vetorReal em ordem crescente
 		Arrays.sort(vetorReal);
-		
-		System.out.println("");
-		
-		System.out.println("Valores ordenados crescente");
-		
-		for(float valor : vetorReal) {
-			System.out.printf("vetorReal = %.2f\n", valor);
+
+		// Exibindo o vetorReal com os dados em ordem crescente
+		for (var valor : vetorReal) {
+			System.out.println("Elemento atual: " + valor);
 		}
-		
-		System.out.println("");
-		
+
+		// Ordenando o vetorReal em ordem decrescente
+		// Previamente ele deve estar ordenado em ordem crescente
 		Arrays.sort(vetorReal, Collections.reverseOrder());
-		
-		System.out.println("Valores ordenados decrescente");
-		
-		for(float valor : vetorReal) {
-			System.out.printf("vetorReal = %.2f\n", valor);
+
+		// Exibindo o vetorReal com os dados em ordem decrescente
+		for (var valor : vetorReal) {
+			System.out.println("Elemento atual: " + valor);
 		}
-		
-		String vetorStrings[] = {"Chihuahua", "Caramelo", "Spitz Alemão", "Border Collie", "Husky"};
-		
+
+		// Ordenando o vetorStrings em ordem crescente
 		Arrays.sort(vetorStrings);
-		
-		System.out.println("");
-		System.out.println("Valores ordenados");
-		
-		for(var cachorro : vetorStrings) {
-			System.out.println("Raça cachorrinho: " + cachorro);
+
+		// Exibindo o vetorStrings com os dados em ordem crescente
+		for (var valor : vetorStrings) {
+			System.out.println("Elemento atual: " + valor);
 		}
-		
+
 		leia.close();
 	}
 
